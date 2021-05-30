fdbff97dcb4b2778742901b6264028708329a197
unknown <Daniel AG>
2021-05-30 14:50:23 -0300

    'Readme atualizado'

--- Em-breve/lgpd-2.js
+++ Em-breve/lgpd-2.js
@@ -1,15 +1,17 @@
 /*! js-cookie v3.0.0-rc.1 | MIT */
 !function (e, t) { "object" == typeof exports && "undefined" != typeof module ? module.exports = t() : "function" == typeof define && define.amd ? define(t) : (e = e || self, function () { var n = e.Cookies, r = e.Cookies = t(); r.noConflict = function () { return e.Cookies = n, r } }()) }(this, function () { "use strict"; function e(e) { for (var t = 1; t < arguments.length; t++) { var n = arguments[t]; for (var r in n) e[r] = n[r] } return e } var t = { read: function (e) { return e.replace(/(%[\dA-F]{2})+/gi, decodeURIComponent) }, write: function (e) { return encodeURIComponent(e).replace(/%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g, decodeURIComponent) } }; return function n(r, o) { function i(t, n, i) { if ("undefined" != typeof document) { "number" == typeof (i = e({}, o, i)).expires && (i.expires = new Date(Date.now() + 864e5 * i.expires)), i.expires && (i.expires = i.expires.toUTCString()), t = encodeURIComponent(t).replace(/%(2[346B]|5E|60|7C)/g, decodeURIComponent).replace(/[()]/g, escape), n = r.write(n, t); var c = ""; for (var u in i) i[u] && (c += "; " + u, !0 !== i[u] && (c += "=" + i[u].split(";")[0])); return document.cookie = t + "=" + n + c } } return Object.create({ set: i, get: function (e) { if ("undefined" != typeof document && (!arguments.length || e)) { for (var n = document.cookie ? document.cookie.split("; ") : [], o = {}, i = 0; i < n.length; i++) { var c = n[i].split("="), u = c.slice(1).join("="); '"' === u[0] && (u = u.slice(1, -1)); try { var f = t.read(c[0]); if (o[f] = r.read(u, f), e === f) break } catch (e) { } } return e ? o[e] : o } }, remove: function (t, n) { i(t, "", e({}, n, { expires: -1 })) }, withAttributes: function (t) { return n(this.converter, e({}, this.attributes, t)) }, withConverter: function (t) { return n(e({}, this.converter, t), this.attributes) } }, { attributes: { value: Object.freeze(o) }, converter: { value: Object.freeze(r) } }) }(t, { path: "/" }) });
 
-(function (document) {
+document.addEventListener('DOMContentLoaded', function () {
 
-    'use strict';
+    (function (document) {
 
-    var lgpdCookiesAccepted = Cookies.get('lgpd-cookies-accepted') || 'not';
+        'use strict';
 
-    if (lgpdCookiesAccepted != 'yes') {
+        var lgpdCookiesAccepted = Cookies.get('lgpd-cookies-accepted') || 'not';
 
-        document.querySelector('body').insertAdjacentHTML('beforeend', `<div class="lgpd-overlay"
+        if (lgpdCookiesAccepted != 'yes') {
+
+            document.querySelector('body').insertAdjacentHTML('beforeend', `<div class="lgpd-overlay"
 style="position: fixed; top:0; bottom: 0; left: 0; right: 0; background-color: #000; opacity: 0; z-index: 998; transition: opacity ease-in-out 0.7s;">
 </div>
 <div class="lgpd-container"
@@ -134,115 +136,117 @@ style="position: fixed; top: 1.5rem; bottom: 1.5rem; left: 1.5rem; right: 1.5rem
 </div>
 </div>`)
 
-        setTimeout(function () {
-            var notEssentilAuthorized = true;
-            var moreOptionsSliding = false;
-            var cookieTypeSlindg = false;
-            var elementOverlay = document.querySelector('.lgpd-overlay');
-            var elementContainer = document.querySelector('.lgpd-container');
-            var elementContent = document.querySelector('.lgpd-content');
+            setTimeout(function () {
+                var notEssentilAuthorized = true;
+                var moreOptionsSliding = false;
+                var cookieTypeSlindg = false;
+                var elementOverlay = document.querySelector('.lgpd-overlay');
+                var elementContainer = document.querySelector('.lgpd-container');
+                var elementContent = document.querySelector('.lgpd-content');
 
-            elementOverlay.style.opacity = '0.7'
-            elementContent.style.opacity = '1'
-            elementContent.style.transform = 'scale(1)'
+                elementOverlay.style.opacity = '0.7'
+                elementContent.style.opacity = '1'
+                elementContent.style.transform = 'scale(1)'
 
-            document.querySelector('.lgpd-accept-trigger').addEventListener('click', function (ev) {
+                document.querySelector('.lgpd-accept-trigger').addEventListener('click', function (ev) {
 
-                ev.preventDefault();
+                    ev.preventDefault();
 
-                Cookies.set('lgpd-cookies-accepted', 'yes', { expires: 360 })
+                    Cookies.set('lgpd-cookies-accepted', 'yes', { expires: 360 })
 
-                elementOverlay.style.opacity = '0'
-                elementContent.style.opacity = '0'
-                elementContent.style.transform = 'scale(0)'
+                    elementOverlay.style.opacity = '0'
+                    elementContent.style.opacity = '0'
+                    elementContent.style.transform = 'scale(0)'
 
-                setTimeout(function () {
-                    elementOverlay.remove();
-                    elementContainer.remove();
-                }, 500)
-            })
+                    setTimeout(function () {
+                        elementOverlay.remove();
+                        elementContainer.remove();
+                    }, 500)
+                })
 
-            document.querySelectorAll('.lgpd-cookie-type-trigger').forEach(function (el) {
+                document.querySelectorAll('.lgpd-cookie-type-trigger').forEach(function (el) {
 
-                el.addEventListener('click', function (ev) {
+                    el.addEventListener('click', function (ev) {
 
-                    if (false == cookieTypeSlindg) {
+                        if (false == cookieTypeSlindg) {
 
-                        cookieTypeSlindg = true;
+                            cookieTypeSlindg = true;
 
-                        var slide = this.parentNode.parentNode.querySelector('.lgpd-cookie-type-slide');
-                        var state = slide.getAttribute('state') || ''
-                        var arrow = this.querySelector('svg')
+                            var slide = this.parentNode.parentNode.querySelector('.lgpd-cookie-type-slide');
+                            var state = slide.getAttribute('state') || ''
+                            var arrow = this.querySelector('svg')
 
-                        if (state == 'collapsed') {
+                            if (state == 'collapsed') {
 
-                            slide.style.height = slide.querySelector('div:first-child').clientHeight + 'px'
-                            slide.setAttribute('state', 'expanded')
-                            arrow.style.transform = 'rotate(90deg)'
+                                slide.style.height = slide.querySelector('div:first-child').clientHeight + 'px'
+                                slide.setAttribute('state', 'expanded')
+                                arrow.style.transform = 'rotate(90deg)'
+
+                            } else {
+
+                                slide.style.height = '0px'
+                                slide.setAttribute('state', 'collapsed')
+                                arrow.style.transform = 'rotate(0deg)'
+                            }
+
+                            setTimeout(function () { cookieTypeSlindg = false }, 600)
+                        }
+                    })
+                })
+
+                document.querySelectorAll('.lgpd-toggle-switch svg').forEach(function (el) {
+
+                    el.addEventListener('click', function (ev) {
+
+                        var classNameClicked = this.getAttribute('class') || ''
+                        var classNameToShow = '.lgpd-toggle-switch-'
+
+                        this.style.display = 'none';
+
+                        notEssentilAuthorized = classNameClicked.indexOf('-enabled') == -1;
+
+                        classNameToShow += notEssentilAuthorized ? 'enabled' : 'disabled';
+
+                        this.parentNode.querySelector(classNameToShow).style.display = 'inherit';
+                    })
+                })
+
+                document.querySelector('.lgpd-more-options-btn').addEventListener('click', function (ev) {
+
+                    ev.preventDefault()
+
+                    if (false == moreOptionsSliding) {
+
+                        moreOptionsSliding = true
+
+                        var symbolContainer = ev.currentTarget.querySelector('span')
+                        var currentSymbol = symbolContainer.textContent || symbolContainer.innerText
+                        var slide = document.querySelector('.lgpd-more-options-slide')
+                        var slideContent = slide.querySelector('div:first-child')
+
+                        if (currentSymbol == '[+]') {
+
+                            slide.style.height = slideContent.clientHeight + 'px'
+                            symbolContainer.innerText = '[-]'
 
                         } else {
 
-                            slide.style.height = '0px'
-                            slide.setAttribute('state', 'collapsed')
-                            arrow.style.transform = 'rotate(0deg)'
+                            slide.style.height = slideContent.clientHeight + 'px'
+                            symbolContainer.innerText = '[+]'
+                            setTimeout(function () { slide.style.height = '0px' }, 100)
                         }
 
-                        setTimeout(function () { cookieTypeSlindg = false }, 600)
+                        setTimeout(function () {
+                            moreOptionsSliding = false
+                            if (currentSymbol == '[+]') {
+                                slide.style.height = 'auto';
+                            }
+                        }, 600)
                     }
                 })
-            })
+            }, 100)
+        }
 
-            document.querySelectorAll('.lgpd-toggle-switch svg').forEach(function (el) {
+    })(document);
 
-                el.addEventListener('click', function (ev) {
-
-                    var classNameClicked = this.getAttribute('class') || ''
-                    var classNameToShow = '.lgpd-toggle-switch-'
-
-                    this.style.display = 'none';
-
-                    notEssentilAuthorized = classNameClicked.indexOf('-enabled') == -1;
-
-                    classNameToShow += notEssentilAuthorized ? 'enabled' : 'disabled';
-
-                    this.parentNode.querySelector(classNameToShow).style.display = 'inherit';
-                })
-            })
-
-            document.querySelector('.lgpd-more-options-btn').addEventListener('click', function (ev) {
-
-                ev.preventDefault()
-
-                if (false == moreOptionsSliding) {
-
-                    moreOptionsSliding = true
-
-                    var symbolContainer = ev.currentTarget.querySelector('span')
-                    var currentSymbol = symbolContainer.textContent || symbolContainer.innerText
-                    var slide = document.querySelector('.lgpd-more-options-slide')
-                    var slideContent = slide.querySelector('div:first-child')
-
-                    if (currentSymbol == '[+]') {
-
-                        slide.style.height = slideContent.clientHeight + 'px'
-                        symbolContainer.innerText = '[-]'
-
-                    } else {
-
-                        slide.style.height = slideContent.clientHeight + 'px'
-                        symbolContainer.innerText = '[+]'
-                        setTimeout(function () { slide.style.height = '0px' }, 100)
-                    }
-
-                    setTimeout(function () {
-                        moreOptionsSliding = false
-                        if (currentSymbol == '[+]') {
-                            slide.style.height = 'auto';
-                        }
-                    }, 600)
-                }
-            })
-        }, 100)
-    }
-
-})(document);
+});
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# lgpd
-Lgpd no seu site. Simples e fácil. Não precisa ser programador para utilizar
+# LGPD
+Inclua LGPD no seu site. Simples e fácil. Não precisa ser programador para utilizar.
+**GRATUITO !!!** Isso mesmo, totalmente de graça.
+
+# Clique [**AQUI**](https://lgpd.ml) para saber como utilizar.
+
+<img src="assets/images/lgpd-capa-github.jpg" alt="Inclua LGPD no seu site de graça">
+
+>**Códigos de terceiros**
+>- https://github.com/js-cookie/js-cookie
